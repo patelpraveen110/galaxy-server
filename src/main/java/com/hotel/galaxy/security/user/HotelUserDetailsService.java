69d69136c8d9bcdc4a5d1f0189386e162be0927e
patelpraveen110 <patelpraveen110@gmail.com>
2024-08-04 18:01:12 +0530

    adding security features

--- src/main/java/com/hotel/galaxy/security/user/HotelUserDetailsService.java
+++ src/main/java/com/hotel/galaxy/security/user/HotelUserDetailsService.java
@@ -1,25 +1,25 @@
-//package com.hotel.galaxy.security.user;
-//
-//import com.hotel.galaxy.model.User;
-//import com.hotel.galaxy.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * @author Simpson Alfred
-// */
-//@Service
-//@RequiredArgsConstructor
-//public class HotelUserDetailsService implements UserDetailsService {
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        User user = userRepository.findByEmail(email)
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//        return HotelUserDetails.buildUserDetails(user);
-//    }
-//}
+package com.hotel.galaxy.security.user;
+
+import com.hotel.galaxy.model.User;
+import com.hotel.galaxy.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author praveen patel
+ */
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
+}
