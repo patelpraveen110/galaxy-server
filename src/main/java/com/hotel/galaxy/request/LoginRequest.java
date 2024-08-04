2cedf7977310eaa078f2ab40713abd18bd1daf03
patelpraveen110 <patelpraveen110@gmail.com>
2024-08-05 00:29:55 +0530

    server side code with security

--- src/main/java/com/hotel/galaxy/request/LoginRequest.java
+++ src/main/java/com/hotel/galaxy/request/LoginRequest.java
@@ -1,15 +1,15 @@
-//package com.hotel.galaxy.request;
-//
-//import jakarta.validation.constraints.NotBlank;
-//import lombok.Data;
-//
-///**
-// * @author Simpson Alfred
-// */
-//@Data
-//public class LoginRequest {
-//    @NotBlank
-//    private String email;
-//    @NotBlank
-//    private String password;
-//}
+package com.hotel.galaxy.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * @author praveen patel
+ */
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
--- src/main/java/com/hotel/galaxy/security/WebSecurityConfig.java
+++ src/main/java/com/hotel/galaxy/security/WebSecurityConfig.java
@@ -18,10 +18,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 /**
- * @author praveen patel
+ * @author Simpson Alfred
  */
+
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
@@ -59,12 +59,10 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/rooms/**","/bookings/**")
-                        .permitAll()
-                        .requestMatchers("/roles/**").hasRole("ADMIN")
+                        .permitAll().requestMatchers("/roles/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
 }
